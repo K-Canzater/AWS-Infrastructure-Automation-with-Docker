e812fb39e9206e118ad45fab285b0e2bddb63411
K-Canzater <moneymaking14223@gmail.com>
2025-05-03 18:18:46 -0400

    Improved Code

--- aws-tools/start/start-instances.py
+++ aws-tools/start/start-instances.py
@@ -7,23 +7,27 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Create EC2 client
-ec2_client = boto3.client('ec2')
+ec2_client = boto3.client("ec2")
+
 
 def start_instances(instance_ids):
     try:
         response = ec2_client.start_instances(InstanceIds=instance_ids)
 
-        for instance in response['StartingInstances']:
+        for instance in response["StartingInstances"]:
             instance_id = instance.get("InstanceId", "Unknown")
             current_state = instance.get("CurrentState", {}).get("Name", "Unknown")
 
-            logger.info(f"Started instance {instance_id} — Current state: {current_state}")
+            logger.info(
+                f"Started instance {instance_id} — Current state: {current_state}"
+            )
     except Exception as e:
         logger.error(f"FAILED TO START INSTANCE: {e}")
 
+
 if __name__ == "__main__":
     ids_env = os.environ.get("INSTANCE_IDS")
-    
+
     if not ids_env:
         logger.error("No INSTANCE_IDS environment variable set.")
         exit(1)
--- aws-tools/stop/stop-instances.py
+++ aws-tools/stop/stop-instances.py
@@ -5,22 +5,30 @@ import os
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-ec2_client = boto3.client('ec2', region_name= 'us-east-1')
+ec2_client = boto3.client("ec2", region_name="us-east-1")
+
 
 def stop_instances(instance_ids):
     try:
         response = ec2_client.stop_instances(InstanceIds=instance_ids)
-        for instance in response['StoppingInstances']:
-            instance_id = instance.get("InstanceId", 'ID Unknown')
-            current_state = instance.get("CurrentState", {}).get("Name", 'State Unknown')
+        for instance in response["StoppingInstances"]:
+            instance_id = instance.get("InstanceId", "ID Unknown")
+            current_state = instance.get("CurrentState", {}).get(
+                "Name", "State Unknown"
+            )
             logger.info(f"Instance ID: {instance_id} | Current State: {current_state}")
     except Exception as e:
         logger.error(f"FAILED TO STOP INSTANCE: {e}")
 
+
 if __name__ == "__main__":
-    ids_str = os.getenv("INSTANCE_IDS")  # Example: "i-02e535df52e47f346,i-0abc123456def7890"
+    ids_str = os.getenv(
+        "INSTANCE_IDS"
+    )  # Example: "i-02e535df52e47f346,i-0abc123456def7890"
+
     if not ids_str:
         logger.error("No INSTANCE_IDS provided. Set the environment variable.")
+
     else:
         instance_ids = [x.strip() for x in ids_str.split(",")]
         stop_instances(instance_ids)
